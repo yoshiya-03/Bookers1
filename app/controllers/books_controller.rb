f74d049eb6f9926b5f56a1d33e91e06a9ea27388
EC2 Default User <ec2-user@ip-172-31-24-99.ec2.internal>
2020-12-06 04:18:31 +0000

    [update]index.html更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,39 @@
+  
 class BooksController < ApplicationController
   
   def index
-    @books = Book.all
+      @books = Book.all
+      @book = Book.new(book_params)
   end
 
   def show
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
-  end 
+       book = Book.new(book_params)
+       book.save
+       redirect_to book_path(book.id)
+  end
 
   def edit
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      redirect_to book_path(book)
   end
-  
+
+  def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_path
+  end
+
   private
   def book_params
-    params.require(:book).permit(:title,  :body)
+      para.permit(:title, :body)
   end
-end
+end 
